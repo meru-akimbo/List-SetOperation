1cac0c9c389cd3ebb8dc4e147fd8ab58ed7d77c5
meru_akimbo <merukatoruayu0@gmail.com>
2015-05-13 00:15:46 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+List::SetOperation - It's new $module
+
+# SYNOPSIS
+
+    use List::SetOperation;
+
+# DESCRIPTION
+
+List::SetOperation is ...
+
+# LICENSE
+
+Copyright (C) meru\_akimbo.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+meru\_akimbo <merukatoruayu0@gmail.com>
---
+++ lib/List/SetOperation.pm
@@ -0,0 +1,39 @@
+package List::SetOperation;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+List::SetOperation - It's new $module
+
+=head1 SYNOPSIS
+
+    use List::SetOperation;
+
+=head1 DESCRIPTION
+
+List::SetOperation is ...
+
+=head1 LICENSE
+
+Copyright (C) meru_akimbo.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+meru_akimbo E<lt>merukatoruayu0@gmail.comE<gt>
+
+=cut
+
